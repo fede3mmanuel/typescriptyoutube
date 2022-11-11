7f6d4ab26e9fd1fc3cec4380bfb169263bf5a2bd
Emmanuel Sanchez <fede@fede.website>
2022-11-11 13:27:22 -0600

    starting functions

---
+++ 01intro/myfunction.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    // return num + 2
+    return 'hello';
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) {
+}
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+var myvalue = addTwo(5);
+getUpper("4");
+signUpUser("Fede", "fede@fede.website", false);
+loginUser("h", "h@h.cm");
---
+++ 01intro/myfunction.ts
@@ -0,0 +1,24 @@
+function addTwo(num: number) {
+    // return num + 2
+    return 'hello'
+}
+
+function getUpper(val: string) {
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {
+    
+}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+let myvalue = addTwo(5)
+
+getUpper("4")
+
+signUpUser("Fede","fede@fede.website", false)
+
+loginUser("h", "h@h.cm")
+
+export {}
