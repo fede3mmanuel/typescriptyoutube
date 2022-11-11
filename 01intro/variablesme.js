654eca571e99ecc6c3d9275b66a3654b7cfe6fcf
Emmanuel Sanchez <fede@fede.website>
2022-11-11 12:24:20 -0600

    autocomplete on typescript

---
+++ 01intro/variablesme.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var greetings = "Hello Fede";
+var mynum = 6;
+greetings.toLowerCase();
+console.log(greetings);
---
+++ 01intro/variablesme.ts
@@ -0,0 +1,8 @@
+let greetings: string = "Hello Fede";
+
+let mynum = 6
+
+greetings.toLowerCase()
+console.log(greetings);
+
+export {}
