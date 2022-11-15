6a03459aca4c0228fa715c212cc5e40b58558351
Emmanuel Sanchez <fede@fede.website>
2022-11-14 18:44:09 -0600

    type aliases

--- myObjects.ts
+++ myObjects.ts
@@ -1,19 +1,32 @@
-const User = {
-    name: "hitesh",
-    email: "fede@fede.website",
-    isActive: false
+// const User = {
+//     name: "hitesh",
+//     email: "fede@fede.website",
+//     isActive: false
+// }
+
+// function createUser ({name: string, isPaid: boolean}){}
+
+// let newUser = {name: "hitesh", isPaid: false, email: "h@h.com" }
+
+// createUser(newUser)
+
+
+
+// function createCourse():{name: string, price: number}{
+//     return {name: "reactjs", price: 399}
+// }
+
+
+type User = {
+    name: string;
+    email: string;
+    isActive: Boolean
 }
 
-function createUser ({name: string, isPaid: boolean}){}
-
-let newUser = {name: "hitesh", isPaid: false, email: "h@h.com" }
-
-createUser(newUser)
-
-
-
-function createCourse():{name: string, price: number}{
-    return {name: "reactjs", price: 399}
+function createUser(user: User): User{
+    return {name: "", email: "", isActive: true}
 }
+
+createUser({name: "", email: "", isActive: true})
 
 export {}
