6df09f1d500982fbe02a64c6ba0a913aa56e3d2d
Emmanuel Sanchez <fede@fede.website>
2022-11-14 20:49:23 -0600

    Tuples

---
+++ myTupEnums.ts
@@ -0,0 +1,16 @@
+// const user: string[] = ["hc"]
+
+let tUser: [string, number, boolean]
+
+tUser = ["hc", 131, true]
+
+
+let rgb: [number, number, number] = [255, 123, 112]
+
+type User = [number, string]
+
+const newUser: User = [12, "12"]
+
+newUser[1] = "fd@ce.com"
+
+newUser.push(1)
