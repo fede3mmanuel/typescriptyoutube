7fa926c4a569950723e9dc4a254e36e553854e4a
Emmanuel Sanchez <fede@fede.website>
2022-11-15 18:02:57 -0600

    Private public

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,10 +1,19 @@
 "use strict";
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
 class User {
     constructor(email, name) {
-        this.city = "Jaipur";
         this.email = email;
         this.name = name;
+        this.city = "Jaipur";
     }
 }
 const hitesh = new User("h@h.com", "hitesh");
-// hitesh.city = "delhi";
+// hitesh.name
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,12 +1,22 @@
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
 class User {
-    email: string
-    name: string
     readonly city: string = "Jaipur"
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
+    constructor(
+        public email: string,
+        public name: string,
+        // private userId: string
+        ) {
     }
 }
 
 const hitesh = new User("h@h.com", "hitesh")
-// hitesh.city = "delhi";
+// hitesh.name
