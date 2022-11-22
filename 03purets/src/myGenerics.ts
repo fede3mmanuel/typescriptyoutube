a90377653fd0819cdf158a384be01524a4a10616
Emmanuel Sanchez <fede@fede.website>
2022-11-22 14:53:08 -0600

    Generics

---
+++ 03purets/src/myGenerics.ts
@@ -0,0 +1,28 @@
+const score: Array<number> = []
+
+const names: Array<string> = []
+
+function identityOne(val: number | boolean): boolean | number {
+    return val
+}
+
+function identityTwo(val: any): any {
+    return val
+}
+
+function identityThree<Type>(val: Type): Type {
+    return val
+}
+
+// identityThree(true)
+
+function identityFour<T>(val: T): T {
+    return val
+}
+
+interface Bootle{
+    brand: string,
+    type: number
+}
+
+identityFour<Bootle>("", 2)
