e0de5914d5c619802611261a835a6b85a313e000
Emmanuel Sanchez <fede@fede.website>
2022-11-23 11:16:04 -0600

    Discriminated Union and Exhaustiveness Checking with never

--- 03purets/dist/detection.js
+++ 03purets/dist/detection.js
@@ -38,3 +38,20 @@ function getFood(pet) {
         return "Bird Food";
     }
 }
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        default:
+            const _defaultForShape = shape;
+            return _defaultForShape;
+    }
+}
