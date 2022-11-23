9069f1bd19c3f67052c0511007fa63eedea490ed
Emmanuel Sanchez <fede@fede.website>
2022-11-23 11:02:08 -0600

    instance of and Type Predicates

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -29,3 +29,29 @@ function isAdminAccount(account: User | Admin) {
         return account.isAdmin
     }
 }
+
+function logValues(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+        
+    }
+}
+
+type Fish = {swim: ( ) => void};
+type Bird = {fly: ( ) => void};
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird) {
+    if (isFish(pet)) {
+        pet
+        return "fish food"
+    } else {
+        pet
+        return "Bird Food"
+    }
+}
